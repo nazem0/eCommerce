2160ddf00f5bedf431c74d4b019f5423cb8c8de8
nazem0 <mohamednazem09@gmail.com>
2023-09-08 19:47:30 +0300

    commit

--- src/app/Components/product/product.component.ts
+++ src/app/Components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/Models/i-product';
+import { ProductsApiService } from 'src/app/Services/products-api.service';
 
 @Component({
   selector: 'app-product',
@@ -7,10 +9,30 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-  orderby: string = "";
-  constructor(private route: ActivatedRoute) {
+  Product:IProduct|null=null;
+  // Product:IProduct={
+  //   id:2,
+  //   title:"test",
+  //   image:"",
+  //   category:"",
+  //   description:"",
+  //   price:2,
+  //   rating:{
+  //     rate:2,
+  //     count:2
+  //   }
+  // }
+  constructor(private route: ActivatedRoute,private ProductApi:ProductsApiService) {
 
   }
   ngOnInit() {
+    this.route.paramMap.subscribe((params)=>{
+      let ProductId=Number(params.get("id"));
+      this.ProductApi.GetProductById(ProductId).subscribe((response:IProduct)=>
+      {
+        this.Product=response;
+      })
+    })
+    
   }
 }
--- src/app/Services/products-api.service.ts
+++ src/app/Services/products-api.service.ts
@@ -1,28 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IProduct } from '../Models/i-product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsApiService {
-  private AllProducts :string = 'https://fakestoreapi.com/products/';
-  public SelectedCategory :string = ''
-  public SelectedProduct: number=-1;
-  private SelectedCategoryURI:string=`https://fakestoreapi.com/products/category/${this.SelectedCategory}`
-  private SelectedProductURI:string=`https://fakestoreapi.com/products/${this.SelectedProduct}`
-  // private Products:IProduct[]=[];
+  private AllProducts: string = 'https://fakestoreapi.com/products/';
+  private SelectedProduct:any;
   constructor(private httpClient: HttpClient) { }
 
-  GetProducts():any{
+  GetProducts(): any {
     return this.httpClient.get(this.AllProducts)
   }
 
-  GetProductsByCategory(category:string):any{
-    this.SelectedCategory = category;
-    return this.httpClient.get(this.SelectedCategoryURI)
+  GetProductsByCategory(category: string): any {
+    // this.SelectedCategory = category;
+    // return this.httpClient.get(this.SelectedCategoryURI)
   }
-  GetProductById(id:number):any{
-    this.SelectedProduct = id;
-    return this.httpClient.get(this.SelectedProductURI)
+  GetProductById(id: number): any {
+    return this.httpClient.get(`https://fakestoreapi.com/products/${id}`)
   }
 }
